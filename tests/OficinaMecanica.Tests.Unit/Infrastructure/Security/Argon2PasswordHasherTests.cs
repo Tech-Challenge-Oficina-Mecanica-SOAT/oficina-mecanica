8e888f61c2c6f2e1af1d9510bd026ac6bd4479e8
vinolivae <viniciusoa@outlook.com>
2026-05-17 12:11:19 -0300

    test: edge cases de hash malformado em Argon2PasswordHasher

--- tests/OficinaMecanica.Tests.Unit/Infrastructure/Security/Argon2PasswordHasherTests.cs
+++ tests/OficinaMecanica.Tests.Unit/Infrastructure/Security/Argon2PasswordHasherTests.cs
@@ -51,4 +51,14 @@ public class Argon2PasswordHasherTests
     {
         _sut.Verificar("qualquer", "lixo-invalido").Should().BeFalse();
     }
+
+    [Theory]
+    [InlineData("$argon2i$v=19$m=9216,t=4,p=1$salt$hash")]
+    [InlineData("$argon2id$v=19$m=9216,t=4$salt$hash")]
+    [InlineData("$argon2id$v=19$m=9216,t=4,p=1$###$hash")]
+    [InlineData("$argon2id$v=19$m=9216,t=4,p=1$YWJjMTIz$###")]
+    public void Verificar_RetornaFalseParaHashMalformadoVariadosCasos(string hashInvalido)
+    {
+        _sut.Verificar("qualquer", hashInvalido).Should().BeFalse();
+    }
 }
