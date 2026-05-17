2a4ded80b3b9e7d4aad9be891903776aac822f6f
vinolivae <viniciusoa@outlook.com>
2026-05-17 12:08:56 -0300

    test: cobertura unitaria de use cases de OrdemServicoStatus

---
+++ tests/OficinaMecanica.Tests.Unit/UseCases/OrdemServicoStatus/MarcarAguardandoAprovacao/MarcarAguardandoAprovacaoUseCaseTests.cs
@@ -0,0 +1,48 @@
+using FluentAssertions;
+using Moq;
+using OficinaMecanica.Application.Common;
+using OficinaMecanica.Application.UseCases.OrdemServicoStatus.MarcarAguardandoAprovacao;
+using OficinaMecanica.Domain.Entities;
+using OficinaMecanica.Domain.Interfaces;
+
+namespace OficinaMecanica.Tests.Unit.UseCases.OrdemServicoStatus.MarcarAguardandoAprovacao;
+
+public class MarcarAguardandoAprovacaoUseCaseTests
+{
+    private readonly Mock<IOrdemServicoRepository> _repo = new();
+    private readonly Mock<IDomainEventDispatcher> _dispatcher = new();
+    private readonly MarcarAguardandoAprovacaoUseCase _sut;
+
+    public MarcarAguardandoAprovacaoUseCaseTests() =>
+        _sut = new MarcarAguardandoAprovacaoUseCase(_repo.Object, _dispatcher.Object);
+
+    [Fact]
+    public async Task ExecutarAsync_OSNaoEncontrada_RetornaNotFound()
+    {
+        _repo.Setup(r => r.ObterPorIdAsync(It.IsAny<Guid>())).ReturnsAsync((Domain.Entities.OrdemServico?)null);
+        var result = await _sut.ExecutarAsync(new MarcarAguardandoAprovacaoRequest(Guid.NewGuid(), "user"));
+        result.ErrorType.Should().Be(ResultErrorType.NotFound);
+    }
+
+    [Fact]
+    public async Task ExecutarAsync_TransicaoInvalida_RetornaValidation()
+    {
+        var os = new Domain.Entities.OrdemServico(Guid.NewGuid(), Guid.NewGuid(), "obs");
+        _repo.Setup(r => r.ObterPorIdAsync(os.Id)).ReturnsAsync(os);
+        var result = await _sut.ExecutarAsync(new MarcarAguardandoAprovacaoRequest(os.Id, "user"));
+        result.ErrorType.Should().Be(ResultErrorType.Validation);
+    }
+
+    [Fact]
+    public async Task ExecutarAsync_Valido_TransitaParaAguardandoAprovacao()
+    {
+        var os = new Domain.Entities.OrdemServico(Guid.NewGuid(), Guid.NewGuid(), "obs");
+        os.ForcarStatus(EnumStatusOS.EmDiagnostico, "admin", "setup");
+        _repo.Setup(r => r.ObterPorIdAsync(os.Id)).ReturnsAsync(os);
+
+        var result = await _sut.ExecutarAsync(new MarcarAguardandoAprovacaoRequest(os.Id, "user"));
+
+        result.IsSuccess.Should().BeTrue();
+        os.StatusOS.Should().Be(EnumStatusOS.AguardandoAprovacao);
+    }
+}
---
+++ tests/OficinaMecanica.Tests.Unit/UseCases/OrdemServicoStatus/NotificarConclusao/NotificarConclusaoUseCaseTests.cs
@@ -0,0 +1,41 @@
+using FluentAssertions;
+using Moq;
+using OficinaMecanica.Application.Common;
+using OficinaMecanica.Application.Interfaces;
+using OficinaMecanica.Application.UseCases.OrdemServicoStatus.NotificarConclusao;
+using OficinaMecanica.Domain.Entities;
+using OficinaMecanica.Domain.Interfaces;
+
+namespace OficinaMecanica.Tests.Unit.UseCases.OrdemServicoStatus.NotificarConclusao;
+
+public class NotificarConclusaoUseCaseTests
+{
+    private readonly Mock<IOrdemServicoRepository> _repo = new();
+    private readonly Mock<INotificacaoService> _notificacao = new();
+    private readonly Mock<IAppLogger<NotificarConclusaoUseCase>> _logger = new();
+    private readonly NotificarConclusaoUseCase _sut;
+
+    public NotificarConclusaoUseCaseTests() =>
+        _sut = new NotificarConclusaoUseCase(_repo.Object, _notificacao.Object, _logger.Object);
+
+    [Fact]
+    public async Task ExecutarAsync_OSNaoEncontrada_RetornaNotFound()
+    {
+        _repo.Setup(r => r.ObterPorIdAsync(It.IsAny<Guid>())).ReturnsAsync((Domain.Entities.OrdemServico?)null);
+        var result = await _sut.ExecutarAsync(new NotificarConclusaoRequest(Guid.NewGuid(), "user"));
+        result.ErrorType.Should().Be(ResultErrorType.NotFound);
+    }
+
+    [Fact]
+    public async Task ExecutarAsync_Valido_FinalizaOS()
+    {
+        var os = new Domain.Entities.OrdemServico(Guid.NewGuid(), Guid.NewGuid(), "obs");
+        os.ForcarStatus(EnumStatusOS.EmExecucao, "admin", "setup");
+        _repo.Setup(r => r.ObterPorIdAsync(os.Id)).ReturnsAsync(os);
+
+        var result = await _sut.ExecutarAsync(new NotificarConclusaoRequest(os.Id, "user"));
+
+        result.IsSuccess.Should().BeTrue();
+        os.StatusOS.Should().Be(EnumStatusOS.Finalizada);
+    }
+}
